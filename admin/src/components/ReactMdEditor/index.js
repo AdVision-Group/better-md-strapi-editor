158dc31171883d1785645d0b3a16909e83946d54
flagg2 <samo.wittlinger@gmail.com>
2022-12-12 13:36:48 +0100

    test

--- admin/src/components/ReactMdEditor/index.js
+++ admin/src/components/ReactMdEditor/index.js
@@ -145,6 +145,14 @@ const Editor = ({
               name: "center",
               keyCommand: "center",
               buttonProps: { "aria-label": "Insert title3" },
+              icon: (
+                <svg width="12" height="12" viewBox="0 0 20 20">
+                  <path
+                    fill="currentColor"
+                    d="M15 9c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm4-7H1c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 13l-6-5-2 2-4-5-4 8V4h16v11z"
+                  ></path>
+                </svg>
+              ),
               execute: (state, api) => {
                 const modifyText = `
                 
