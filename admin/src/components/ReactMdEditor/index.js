7282ff3f81f1d5b95580dc005745e3549a9715b7
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-02-13 15:52:52 +0100

    fix: underline icon size

--- admin/src/components/ReactMdEditor/index.js
+++ admin/src/components/ReactMdEditor/index.js
@@ -143,8 +143,8 @@ const Editor = ({
               buttonProps: { "aria-label": "Underline" },
               icon: (
                 <svg
-                  width="800px"
-                  height="800px"
+                  width="12"
+                  height="12"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
